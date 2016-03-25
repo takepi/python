b8ac00e8420702c31432338de59cdd3780fbfa88
takeshi <jagabataa0141@gmail.com>
2016-03-25 17:15:08 +0900

    3/25 部活成果

--- camera/brobokai.py
+++ camera/brobokai.py
@@ -18,7 +18,7 @@ gpio.setup(19, gpio.IN, pull_up_down = gpio.PUD_UP)
 
 def sendData(q):
 	import controller
-	bus = smbus.SMBus(1)	
+	bus = smbus.SMBus(1)
 	gpio.output(21, False)
 	gpio.output(23, False)
 	flg = "notauto"
@@ -26,15 +26,34 @@ def sendData(q):
 	
 	while True:
 		if flg == "notauto":
+			if q.qsize() != 0:
+				if q.get() == "auto":
+					flg = "auto"
+				elif q.get() == "notauto":
+					flg = "notauto"
 			data = controller.read() << 4
+			if data == 0xf0:
+				print "fire"
+				gpio.output(21, True)
+				time.sleep(0.5)
+				gpio.output(21, False)
+				time.sleep(0.5)
+				gpio.output(23, True)
+				time.sleep(0.5)
+				gpio.output(23, False)
+				time.sleep(0.5)
+				data = 12
+				
 		elif flg == "auto":
-			if q.empty():
+			if q.qsize() == 0:
 				data = bdata
 			else:
-				if q.get() == "auto"
+				if q.get() == "auto":
 					flg = "auto"
-				elif q.get() == "notauto"
+				elif q.get() == "notauto":
 					flg = "notauto"
+				elif q.get() == "brake":
+					data = 0
 				elif q.get() == "cw":
 					data = 8
 				elif q.get() == "ccw":
@@ -52,7 +71,7 @@ def sendData(q):
 				else:
 					data = bdata
 		
-		#print bin(data)
+		print bin(data)
 		try:
 			bus.write_byte(0x08, data)
 		except:
@@ -76,7 +95,8 @@ def autoFire(abe, pole, q_debug,q_turret, teamcolor, polenum):
 	flg = 0
 	nflg = 0
 	swing = swing[polenum]
-	
+	q_turret.put("auto")
+
 	if not 50 < swing < 205:
 		if swing >= 205:
 			turret = ["ccw", "cw"]
@@ -85,12 +105,14 @@ def autoFire(abe, pole, q_debug,q_turret, teamcolor, polenum):
 			
 	while abe.GetData() != swing:
 		q_debug.put([abe.GetData()])
-			if abe.GetData < swinig:
-				q_debug.put([turret[0])
-				q_turret.put(turret[0])
-			elif abe.GetData() > swing:
-				q_turret.put([turret[1])
-				q_turret.put(turret[1])
+		if abe.GetData() < swing:
+			q_debug.put([turret[0]])
+			q_turret.put(turret[0])
+		elif abe.GetData() > swing:
+			q_debug.put([turret[1]])
+			q_turret.put(turret[1])
+
+	q_turret.put("brake")
 
 	while True:
 		ret, frame = cap.read()
@@ -102,19 +124,20 @@ def autoFire(abe, pole, q_debug,q_turret, teamcolor, polenum):
 				flg = flg + 1
 				if flg > 20:
 					if teamcolor == bcolor:
-						zure = (point[0][0] - img.shape[1]/2)/16
+						zure = (point[0][0] - img.shape[1]/2 - 20)/16
 						while abe.GetData() != swing + zure:
-							if zure > 0:
+							if abe.GetData() - (swing + zure) < 0:
 								q_debug.put(["cw", abe.GetData(), swing + zure])
 								q_turret.put("cw")
 							else:
 								q_debug.put(["ccw", abe.GetData(), swing + zure])
 								q_turret.put("ccw")
+						q_turret.put("brake")
 						q_debug.put(["turret ok"])
 						q_turret.put("fire")
-						time.sleep(5)
 						break
 					else:
+						q_turret.put("brake")
 						q_debug.put(["turret no"])
 						break
 			else:
@@ -150,7 +173,6 @@ def main():
 			pass
 			#q_debug.put(["not auto"])
 
-		q_turret.put("auto")
 		for i in [3, 2, 1, 3, 2, 1]:
 			autoFire(abe, pole, q_debug, q_turret, teamcolor, i)
 			time.sleep(5)
