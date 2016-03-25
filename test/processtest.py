71ef90f7b11a02a924fba1eb698863ab2a7e15e9
takeshi <jagabataa0141@gmail.com>
2016-03-25 22:39:37 +0900

    Queue追加

--- test/processtest.py
+++ test/processtest.py
@@ -1,23 +1,27 @@
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 import time
 
 class Aaa(Process):
 
-	def __init__(self, aa):
+	def __init__(self, aa, q):
 		Process.__init__(self)
 		self.aa = aa
+		self.q = q
 		self.daemon = False
 
 	def run(self):
 		print "process start"
 		for i in range(5):
 			print self.aa, i
+		print self.q.get()
 		print "process end"
 
 def main():
 
-	p_aaa = Aaa("aa")
-	
+	q_aaa = Queue(maxsize = 1)
+
+	p_aaa = Aaa("aa", q_aaa)
+	q_aaa.put("avav")
 	p_aaa.start()
 	#p_aaa.daemon = True
 	
