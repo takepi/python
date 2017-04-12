57b4be26b9fb42f5fafd1dcc6a998fd4414cb8fe
takeshi <jagabataa0141@gmail.com>
2017-04-12 17:26:41 +0900

    kimagure

--- fun/kusokora.py
+++ fun/kusokora.py
@@ -1,14 +1,18 @@
 import cv2
 
-face_cascade = cv2.CascadeClassifier("C:\Users\takeshi\Downloads\opencv\build\share\OpenCV\haarcascades\haarcascade_frontalface_default.xml")
+cascadepath = "C:\Users\takeshi\Downloads\opencv\build\share\OpenCV\haarcascades\haarcascade_frontalface_default.xml"
 
-print face_cascade
+face_cascade = cv2.CascadeClassifier(cascadepath)
 
-face = cv2.imread("kato.jpg")
 img = cv2.imread("iwakura.jpg")
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.3, minNeighbors = 1, minSize = (100, 100))
+faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.2, minNeighbors = 1, minSize = (10, 10))
+faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.2, minNeighbors = 1, minSize = (10, 10))
 
-print face
+if len(face) > 0:
+	for rect in face:
+		cv2.rectangle(img, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), (0, 0, 0), thickness = 2)
+		
+cv2.imshow("kao", img)
